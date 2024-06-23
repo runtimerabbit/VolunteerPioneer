2397c93f24e5cef33f6c94bd269a3f659682f748
runtimerabbit <zohair.saiyed@icloud.com>
2024-06-22 19:20:19 -0700

    Full implemented locaiton

--- volunteer-pioneer/app/(tabs)/index.tsx
+++ volunteer-pioneer/app/(tabs)/index.tsx
@@ -14,10 +14,31 @@ const StyledText = styled(Text);
  let hardcodedTextColor = "text-[#f00000]";
 
 export default function HomeScreen() {
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
+  const [location, setLocation] = useState<{location: null | string}>({ location: null });
+  const [errorMsg, setErrorMsg] = useState<string>();
 
-  
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+ 
+      let location = await Location.getCurrentPositionAsync({});
+      // @ts-ignore
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+    console.log(location)
+  }
   return (
     <>
     <Header centerComponent={{ text: 'Volunteer Pioneer', style: { color: '#fff', fontWeight: 'bold', height: 35, fontSize: 25 } }} backgroundColor='#93c47d'>
