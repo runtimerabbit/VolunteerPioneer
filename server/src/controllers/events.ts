5c82dcde2f22bd8aad5f28d8f13decf080ce6a4c
runtimerabbit <106444007+runtimerabbit@users.noreply.github.com>
2024-08-02 01:27:44 +0000

    Worked on controllers and routes

--- server/src/controllers/events.ts
+++ server/src/controllers/events.ts
@@ -6,34 +6,61 @@ const createEvent = async (title: string, description: string, date: string, loc
     ])
     if (error) {
         console.log(error)
-        return { error }
+        return { error, status:500 }
     }
-    return data
+    return {data, status:201}
 }
 
 const getEvents = async () => {
     const { data, error } = await supabase.from("events").select()
     if (error) {
         console.log(error)
-        return { error }
+        return { error, status:500 }
     }
-    return data
+    return {data, status:200}
 }
 
-const deleteEvent = async (eventId: string) => {
+const getEvent = async (id: string) => {
+    const { data, error } = await supabase.from("events").select().eq("id", id)
+    if (error) {
+        console.log(error)
+        return { error, status:500 }
+    }
+    return {data, status:200}
+}
+
+const deleteEvent = async (eventId: string, userId: string) => {
+     const { data:evtdata } = await supabase.from("events").select().eq("id", eventId).single();
+    if (!evtdata) {
+        return {error:"Not Found", status:404}
+    }
+    if (evtdata.creator !== userId) {
+        return { error:"Forbidden", status:403 }
+    }
+    
     const { data, error } = await supabase.from("events").delete().eq("id", eventId)
     if (error) {
         console.log(error)
-        return { error }
+        return { error, status:500 }
     }
-    return data
+    return {data, status:200}
 }
 
-const updateEvent = async (eventId: string, title: string, description: string, date: string, location: string, creator: string) => {
-    const { data, error } = await supabase.from("events").update({ title, description, date, location, creator }).eq("id", eventId)
+const updateEvent = async (eventId: string, title: string, description: string, date: string, location: string, userId: string) => {
+    const { data:evtdata } = await supabase.from("events").select().eq("id", eventId).single();
+    if (!evtdata) {
+        return {error:"Not Found", status:404}
+    }
+    if (evtdata.creator !== userId) {
+        return { error:"Forbidden", status:403 }
+    }
+
+    const { data, error } = await supabase.from("events").update({ title, description, date, location, creator:userId }).eq("id", eventId)
     if (error) {
         console.log(error)
-        return { error }
+        return { error, status:500 }
     }
-    return data
+    return {data, status:200}
 }
+
+export { createEvent, updateEvent, deleteEvent, getEvents, getEvent }
--- server/src/routes/events.ts
+++ server/src/routes/events.ts
@@ -1,23 +1,61 @@
 import { Router } from "express"
 import type { Response, Request } from "express"
+import { getEvents, deleteEvent, createEvent, updateEvent, getEvent } from "../controllers/events"
+import { supabase } from "../util/supabase"
+import { isDate } from "util/types";
 
 function eventRouter() {
     const router: Router = Router();
     router.post("/", async (req: Request, res: Response) => {
-        
+        const {title, description, date, location}: Record <string, string> = req.body;
+        const token = req.get("x-access-token");
+        if (!token){
+            return res.status(401).send({error: "Unauthorized", status:401})
+        }
+        let {data:{user}} = await supabase.auth.getUser(token);
+        if (!user) {
+               return res.status(401).send({error: "Unauthorized", status:401})
+        }
+        let creator = user.id;
+        let event = await createEvent(title, description, date, location, creator);
+        return res.status(event.status).send(event);
+    });
+    router.get("/", async (req: Request, res: Response)=>{
+        let events = await getEvents();
+        return res.status(events.status).send(events)
     });
     router.get("/:id", async (req: Request, res: Response) => {
-        res.send(req.params.id);
-
+        let event = await getEvent(req.params.id)
+        return res.status(event.status).send(event)
     });
     router.delete("/:id", async (req: Request, res: Response) => {
-
+        const token = req.get("x-access-token");
+        if (!token){
+            return res.status(401).send({error: "Unauthorized", status:401})
+        }
+        let {data:{user}} = await supabase.auth.getUser(token);
+        if (!user) {
+               return res.status(401).send({error: "Unauthorized", status:401})
+        }
+        let userID = user.id;
+        let event = await deleteEvent(req.params.id, userID)
+        return res.status(event.status).send(event)
     });
-    router.put("/:id", async (req: Request, res: Response) => {
-
+    router.put("/", async (req: Request, res: Response) => {
+        let token = req.get("x-access-token");
+        if (!token){
+            return res.status(401).send({error: "Unauthorized", status:401})
+        }
+        let {data:{user}} = await supabase.auth.getUser(token);
+        if (!user) {
+               return res.status(401).send({error: "Unauthorized", status:401})
+        }
+        let userId = user.id;
+        const { title, description, date, location, id }: Record <string, string> = req.body;
+        let event = await updateEvent(id, title, description, date, location, userId);
+        return res.status(event.status).send(event)
     });
     return router;
 }
 
 export default eventRouter
-
